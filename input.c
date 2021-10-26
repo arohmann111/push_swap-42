c11990a8cbb867d938eec053b901df1b436966e6
Anna-saray Rohmann <arohmann@1-A-8.42heilbronn.de>
2021-10-26 13:22:44 +0200

    node

--- input.c
+++ input.c
@@ -2,5 +2,12 @@
 
 int	input_check(int ac, char **av)
 {
-	
+	int i;
+
+	i = 0;
+	while (i < ac && ft_isdigit(av[i]) != 0)
+	{
+		
+		i++;
+	}
 }
--- push_swap.h
+++ push_swap.h
@@ -5,6 +5,13 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+typedef struct s_node
+{
+	int		num;
+	struct s_node	*next;
+	struct s_node	*prev;
+} t_node;
+
 int	input_check(int ac, char **av);
 
 #endif

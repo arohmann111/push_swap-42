86b5bd8f128160d3c707b1df6b1d296b1a8fe7ad
Anna Rohmann <anna.sy.roh@gmail.com>
2021-11-19 10:23:37 +0100

    sort big

--- sort_big.c
+++ sort_big.c
@@ -11,18 +11,18 @@ int	*find_l_sequence(t_data *data)
 	if (arr == NULL)
 		return (NULL);
 
-	i = 0;
-	num = data->head_a->num;
-	arr[0] = num;
-	while (i < data->size_a)
+	i = 1;
+	arr[0][0] = data->head_a->num;
+	while (i < data->size_a - 1)
 	{	
 		j = 0;
 		while (j < i)
 		{
-			if (num < data->head_a->next->num && )
-			arr[i][j] = num;
+			if (data->head_a->num < data->head_a->next->num)
+				arr[i][j] = num;
 			j++;
 		}
+		data->head_a = data->head_a->next;
 		i++;
 	}
 
